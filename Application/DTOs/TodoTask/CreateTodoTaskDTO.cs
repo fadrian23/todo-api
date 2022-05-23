c46ad09abf1f0b35621936740ea56edccb40f5c2
Adrian Flis <adrianflis23@gmail.com>
2022-05-23 12:44:32 +0200

    Add CreateTodoTaskDTO

---
+++ Application/DTOs/TodoTask/CreateTodoTaskDTO.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.DTOs.TodoTask
+{
+    public class CreateTodoTaskDTO
+    {
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public DateTimeOffset ExpirationDate { get; set; }
+        public int CompletePercent { get; set; }
+    }
+}
