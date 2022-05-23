9daf17378903fa699c07d56172e62cebdfe2e350
Adrian Flis <adrianflis23@gmail.com>
2022-05-23 13:48:28 +0200

    Add SetPercentTodoTaskDTO with validator

---
+++ Application/DTOs/TodoTask/SetPercentTodoTaskDTO.cs
@@ -0,0 +1,22 @@
+﻿using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.DTOs.TodoTask
+{
+    public class SetPercentTodoTaskDTO
+    {
+        public int CompletePercent { get; set; }
+    }
+
+    public class SetPercentTodoTaskDTOValidator : AbstractValidator<SetPercentTodoTaskDTO>
+    {
+        public SetPercentTodoTaskDTOValidator()
+        {
+            RuleFor(x => x.CompletePercent).InclusiveBetween(1, 100);
+        }
+    }
+}
