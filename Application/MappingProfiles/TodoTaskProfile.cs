ba1dc2563d4a336c8d683bdcc7b40b86d67a2b0e
Adrian Flis <adrianflis23@gmail.com>
2022-05-23 13:13:34 +0200

    Add mapping profile for TodoTask

---
+++ Application/MappingProfiles/TodoTaskProfile.cs
@@ -0,0 +1,21 @@
+﻿using Application.DTOs.TodoTask;
+using Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AutoMapper;
+
+namespace Application.MappingProfiles
+{
+    public class TodoTaskProfile : Profile
+    {
+        public TodoTaskProfile()
+        {
+            CreateMap<CreateTodoTaskDTO, TodoTask>().ReverseMap();
+            CreateMap<TodoTaskDTO, TodoTask>().ReverseMap();
+            CreateMap<UpdateTodoTaskDTO, TodoTask>().ReverseMap();
+        }
+    }
+}
